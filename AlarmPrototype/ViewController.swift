c9040c4caa55b11044fe54fed7ae35105d37bef1
Cameron <quadro@outlook.com.au>
2021-07-30 18:18:48 +1000

    Gradient & button borders

--- AlarmPrototype/ViewController.swift
+++ AlarmPrototype/ViewController.swift
@@ -9,10 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var editButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
